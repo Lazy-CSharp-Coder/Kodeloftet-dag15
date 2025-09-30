8175a404df5060e55699e69d82d8483e1f336fe2
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-09-30 11:16:14 +0200

    test

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-// // Hente et element = Hente
-// const hamburgerImg = document.querySelector("#hamburgerImg");
+const submitButton = document.querySelector("#submitButton");
+console.log(submitButton);
 
-// // Alternativ 1:
-// // Ha en funksjon = Modifiserer
-// // Laget en funksjon som henter navList-elementet og bytter klassenavn for synlig/usynlig
-// function hamburgerToggle() {
-//   console.log("Hi from hamburgerToggle");
+function gatherData() 
+{
+  console.log("Button click - vi er her!!");
+  
 
-//   const navListElement = document.querySelector(".navList"); // Bevist brukt klasse for kun et element med det klassenavnet
-//   console.log(navListElement);
+}
 
-//   // Metode 1: Skrive CSS i JS som blir inline-CSS
-//   navListElement.style.display = "flex";
-//   navListElement.style.flexDirection = "column";
-// }
-
-// // Aktiverer en funksjon = Sender
-// hamburgerImg.addEventListener("click", hamburgerToggle);
+submitButton.addEventListener("click", gatherData);
