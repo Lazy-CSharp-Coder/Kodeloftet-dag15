f7567ea376466f36a6db5df78eddcd94920aedc2
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-09-30 11:32:24 +0200

    tset

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+// H = henter noe
 const submitButton = document.querySelector("#submitButton");
 console.log(submitButton);
 
+// M = Modifiserer noe
 function gatherData() 
 {
   console.log("Button click - vi er her!!");
+
+  // H = Hente elementer
+
+    const greetingInput = document.querySelector("#greetingInput");
+    const greetingInputValue = greetingInput.value;
+    console.log(greetingInput);
+    console.log(greetingInputValue);
+
+    const displayFormInputs = document.querySelector("#displayFormInput");
   
+  // M = Modifisere elementer
+
+    displayFormInputs.textContent = greetingInputValue;
+    
+
+  // S = Sende elementer
 
 }
+
+// Send = sender noe
 
 submitButton.addEventListener("click", gatherData);
